833d9b152285664fb161ef7ace851d2a516bed4b
mgulati3 <mgulati3@asu.edu>
2025-02-09 23:08:21 -0700

    Initial commit - added Emaily project

--- server/index.js
+++ server/index.js
@@ -1,11 +1,30 @@
 const express = require('express'); 
+const mongoose = require('mongoose');
+const cookieSession = require('express-session');
+const passport = require('passport');
+const keys = require('./config/keys'); 
+require('./models/user');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({bye : 'buddy'}); 
-});
+app.use(
+    cookieSession({
+        secret: keys.cookieKey,
+        resave: false,
+        saveUninitialized: false,
+        cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 } // 30 days
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app); 
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT); 
 
 // http://localhost:5001/
+// https://localhost:5001/auth/google
